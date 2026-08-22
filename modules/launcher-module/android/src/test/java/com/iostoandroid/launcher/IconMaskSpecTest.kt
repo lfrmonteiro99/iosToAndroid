9c8c3749ccfc3a4a333522232f50a3d78ff19f7e
qa-implementer <qa@local>
2026-08-22 01:07:10 +0100

    fix/482: feat/482: expõe a forma dos ícones (squircle/circle/rounded/original) e o expoente nas definições, com a forma na chave da cache de ícones

--- modules/launcher-module/android/src/test/java/com/iostoandroid/launcher/IconMaskSpecTest.kt
+++ modules/launcher-module/android/src/test/java/com/iostoandroid/launcher/IconMaskSpecTest.kt
@@ -102,7 +102,7 @@ class IconMaskSpecTest {
     fun `a shape key with path separators cannot escape the icons directory`() {
         val name = IconCache.fileName("com.a", 1L, "../../etc/passwd")
         assertFalse(name.contains("/"))
-        assertEquals("com.a_1_......etc.passwd.png", name)
+        assertEquals("com.a_1_..-..-etc-passwd.png", name)
     }
 
     @Test
